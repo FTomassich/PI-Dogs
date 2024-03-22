50fe219e01b43f6a4622660f5127da81803f1728
Franco Tomassich <francotomassich@gmail.com>
2024-03-22 16:33:40 -0300

    styles

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Card from '../Card/Card';
 import Paginado from '../Paginado/Paginado';
@@ -12,163 +12,187 @@ import SearchBar from '../SearchBar/SearchBar';
 
 
 const Home = () => {
-    
-    const dispatch=useDispatch()
-    const allDogs= useSelector((state) => state.dogs) //me traigo el estado del reducer
-    const allTemps=useSelector((state)=>state.temperaments);
-    
-    const [orden, setOrden]=useState('')
-    //Estados locales del paginado
-const [currentPage, setCurrentPage]=useState(1)//mi página actual que arranca en 1
-const [dogsPerPage, setDogsPerPage]=useState(8)//dogs per page (8)
-const indexOfLastDog = currentPage * dogsPerPage //8
-const indexOfFirstDog = indexOfLastDog - dogsPerPage//0
-const currentDogs = allDogs.slice(indexOfFirstDog, indexOfLastDog) //esta constante tiene los perros de la página actual
+
+  const dispatch = useDispatch()
+  const allDogs = useSelector((state) => state.dogs) //me traigo el estado del reducer
+  const allTemps = useSelector((state) => state.temperaments);
+
+  const [orden, setOrden] = useState('')
+  //Estados locales del paginado
+  const [currentPage, setCurrentPage] = useState(1)//mi página actual que arranca en 1
+  const [dogsPerPage, setDogsPerPage] = useState(8)//dogs per page (8)
+  const indexOfLastDog = currentPage * dogsPerPage //8
+  const indexOfFirstDog = indexOfLastDog - dogsPerPage//0
+  const currentDogs = allDogs.slice(indexOfFirstDog, indexOfLastDog) //esta constante tiene los perros de la página actual
 
 
 
-const paginado = (pageNumber) => {
+  const paginado = (pageNumber) => {
     setCurrentPage(pageNumber)
-}
+  }
 
-    useEffect(()=>{
+  useEffect(() => {
 
-      if (allDogs.length === 0) {
-      
-        dispatch (getDogs());
-        dispatch (getTemps());
+    if (allDogs.length === 0) {
+
+      dispatch(getDogs());
+      dispatch(getTemps());
     }
-    
-    
+
+
   }, [dispatch]);
 
-    //Handler de botón refreshDogs 
-    function handleClick(e){
-        e.preventDefault();
-        dispatch (getDogs());
-    }
+  //Handler de botón refreshDogs 
+  function handleClick(e) {
+    e.preventDefault();
+    dispatch(getDogs());
+  }
 
 
-    //Handler para filtrar por temps según el select
+  //Handler para filtrar por temps según el select
 
-    function handleFilterTemps(e) {
-        const selectedTemperament = e.target.value;
-        dispatch(filterByTemps(selectedTemperament));
-        setCurrentPage(1);
-      }
-
-
-    //Handler del select para filtrar según orígen
-    function handleFilterOrigin(e){
-        dispatch(filterOrigin(e.target.value)) //Le paso a la función lo que viene del select ( eso es el: e.target.value)
-        setCurrentPage(1);
-    
-      }
-
-    function handleSort(e){
-        e.preventDefault();
-        dispatch(orderByName(e.target.value))
-        setCurrentPage(1);
-        setOrden( `Ordenado ${e.target.value}`)
-      };
+  function handleFilterTemps(e) {
+    const selectedTemperament = e.target.value;
+    dispatch(filterByTemps(selectedTemperament));
+    setCurrentPage(1);
+  }
 
 
-      function handleOrderByWeight (e){
-        e.preventDefault();
-        dispatch(orderByWeight(e.target.value))
-        setCurrentPage(1);
-        setOrden (`Ordenado ${e.target.value}`)
+  //Handler del select para filtrar según orígen
+  function handleFilterOrigin(e) {
+    dispatch(filterOrigin(e.target.value))
+    setCurrentPage(1);
 
-      
-      }
+  }
+
+  function handleSort(e) {
+    e.preventDefault();
+    dispatch(orderByName(e.target.value))
+    setCurrentPage(1);
+    setOrden(`Ordenado ${e.target.value}`)
+  };
+
+
+  function handleOrderByWeight(e) {
+    e.preventDefault();
+    dispatch(orderByWeight(e.target.value))
+    setCurrentPage(1);
+    setOrden(`Ordenado ${e.target.value}`)
+
+
+  }
 
   return (
-    
-    <div className={style.containerHome}> 
-    <div className={style.containerbutton}>
-    
-     
-    <button className={style.button2}><Link to= '/dogs'>➕ Create Dog</Link>  </button>
 
-   
+    <div className={style.containerHome}>
+      <div className={style.containerbutton}>
+
+
+        <button className={style.button2}><Link to='/dogs'>➕ Create Dog</Link>  </button>
+
+
         <div className={style.containerSearchBar}>
-        <SearchBar></SearchBar>
-    </div>
-   
+          <SearchBar></SearchBar>
+        </div>
 
-    <button className={style.button} onClick={e=>{handleClick(e)}}>Refresh Dogs🐶</button>
 
-    </div>
+        <button className={style.button} onClick={e => { handleClick(e) }}>Refresh Dogs🐶</button>
 
-    
-    
-    <div className={style.containerAllSelect}>
+      </div>
 
-<div className={style.container}>
-    <div className={style.select}>
-    
-        <h3 className={style.h3}>Origin filter</h3>
-    
-            <select onChange={e=>handleFilterOrigin(e)}>
-            <option disabled defaultValue>Filtrar según orígen</option>
-          <option value="All">Todos</option>
-          <option value="created">Creado en base de datos</option>
-          <option value="api">Existente en Api</option>
+
+
+      <div className={style.containerAllSelect}>
+
+
+        <div className={style.group}>
+
+          <div className={style.containerTxt}>
+            <h3 className={style.h3}>Origin filter</h3>
+          </div>
+
+          <div className={style.containerTxt}>
+            <select
+              className={style.select}
+              onChange={e => handleFilterOrigin(e)}>
+              <option disabled defaultValue>Filtrar según orígen</option>
+              <option value="All">Todos</option>
+              <option value="created">Creado en base de datos</option>
+              <option value="api">Existente en Api</option>
             </select>
+          </div>
         </div>
 
-       <div className={style.select}>
-       <h3 className={style.h3}>Alfabetic order</h3>
-        <select onChange={e=>handleSort(e)}>
-        <option disabled defaultValue>Ordenar alfabéticamente</option>
-        <option value="asc">Ascendente (A-Z)</option>
-        <option value="desc">Descendente (Z-A)</option>
-        </select>
-        </div>
+        <div className={style.group}>
+          <div className={style.containerTxt}>
+          <h3 className={style.h3}>Alfabetic order</h3>
+          </div>
+          <div className={style.containerTxt}>
+          <select
+            className={style.select}
+            onChange={e => handleSort(e)}>
+            <option disabled defaultValue>Ordenar alfabéticamente</option>
+            <option value="asc">Ascendente (A-Z)</option>
+            <option value="desc">Descendente (Z-A)</option>
+          </select>
+          </div>
+
 
         </div>
-<div className={style.container}>
-        <div className={style.select}>
-        <h3 className={style.h3}>Order by weight</h3>
-            <select onChange={e=>handleOrderByWeight(e)}>
+
+        <div className={style.group}>
+          <div className={style.containerTxt}>
+          <h3 className={style.h3}>Order by weight</h3>
+          </div>
+          <div className={style.containerTxt}>
+          <select
+            className={style.select}
+            onChange={e => handleOrderByWeight(e)}>
             <option disabled defaultValue>Ordenar por Peso</option>
-          <option value="asc">Ascendente🔼</option>
-          <option value="desc">Descendente🔽</option>
-            </select>
-            </div>
+            <option value="asc">Ascendente🔼</option>
+            <option value="desc">Descendente🔽</option>
+          </select>
+          </div>
+        </div>
 
-            <div className={style.select}>
+        <div className={style.group}>
+          <div className={style.containerTxt}>
             <h3 className={style.h3}>Temps filter</h3>
-            <select onChange={(e)=>handleFilterTemps(e)}>
-  {allTemps.map((temperament) => (
-    <option key={temperament.id} value={temperament.name}>
-      {temperament.name}
-    </option>
-  ))}
-</select>
-
             </div>
-            </div>
+            <div className={style.containerTxt}>
+          <select
+            className={style.select}
+            onChange={(e) => handleFilterTemps(e)}>
+            {allTemps.map((temperament) => (
+              <option key={temperament.id} value={temperament.name}>
+                {temperament.name}
+              </option>
+            ))}
+          </select>
+          </div>
 
-        
-    </div>
 
-    <div className= {style.containerCards}>
+        </div>
 
-        {currentDogs && currentDogs.map((el)=>(
-        <Card id={el.id} name={el.name} image={el.image} weight={el.weight} temperament={el.temperament}/>
+
+      </div>
+
+      <div className={style.containerCards}>
+
+        {currentDogs && currentDogs.map((el) => (
+          <Card id={el.id} name={el.name} image={el.image} weight={el.weight} temperament={el.temperament} />
         ))}
-        
-    </div>
 
-    <Paginado
+      </div>
+
+      <Paginado
         dogsPerPage={dogsPerPage}
         allDogs={allDogs.length}
         currentPage={currentPage}
         paginado={paginado}
-        />
-    
-    </div>//Cierre containerHome
+      />
+
+    </div>
   )
 }
 
