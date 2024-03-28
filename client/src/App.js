48f8293e45a18b405e3c7ef1ba6b752238beb30c
Franco Tomassich <francotomassich@gmail.com>
2024-03-28 00:28:27 -0300

    rt

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './components/Landing/Landing';
 import Home from './components/Home/Home';
