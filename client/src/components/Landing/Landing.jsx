ea311bd9c4b644da38b40fba348317b7e9ea1c39
Franco Tomassich <francotomassich@gmail.com>
2024-04-03 01:58:20 -0300

    landing

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,23 +1,20 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import style from '../Landing/Landing.module.css';
 
 const Landing = () => {
   return (
-  <div className={style.landingcontainer}>
-    <div className={style.itemscontainer}>
-      <div className= {style.text}>
-        <h2>¡Welcome to the Dogs Single Page App!</h2>
-        {/* <h3 >"Life is better with a dog by your side, teaching you lessons of unconditional love, loyalty, and joy in the simplest moments." - Unknown</h3> */}
-        </div>
-         <div className={style.containerbutton}>
-          <Link to= "/home">
-        <button className={style.button}>Ingresar</button> 
-        </Link> 
-        
-        </div>
-        </div>
-    </div>)
-}
+    <div className={style.container}>
+      <h1 className={style.h1}>¡Welcome to Dogs App!</h1>
+      <h2 className={style.h2}>Find your next pet</h2>
+      <div className={style.containerbutton}>
+        <Link to="/home">
+          <button className={style.button}>Start</button>
+        </Link>
+      </div>
+      <div className={style.image}></div>
+    </div>
+    )
+  }
 
 export default Landing
