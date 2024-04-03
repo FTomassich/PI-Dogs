6ff5f910810e73210d821bd37f7f0c2f88d594a8
Franco Tomassich <francotomassich@gmail.com>
2024-04-03 02:14:12 -0300

    landing cambios

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -5,8 +5,8 @@ import style from '../Landing/Landing.module.css';
 const Landing = () => {
   return (
     <div className={style.container}>
-      <h1 className={style.h1}>¡Welcome to Dogs App!</h1>
-      <h2 className={style.h2}>Find your next pet</h2>
+      <h1 className={style.h1}>¡Welcome to Dogs APP!</h1>
+      <h2 className={style.h2}>Find your next friend</h2>
       <div className={style.containerbutton}>
         <Link to="/home">
           <button className={style.button}>Start</button>
